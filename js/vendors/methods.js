d983f13aa0a5ee1d3e1ff53788761bee425f33fc
BrandonTovar <105744366+BrandonTovar@users.noreply.github.com>
2023-02-14 13:50:53 -0600

    Add files via upload

--- js/vendors/methods.js
+++ js/vendors/methods.js
@@ -15,9 +15,6 @@ export function loadProducts(elements) {
 
 export function viewNext(element = document, c = "", i=0, clientWidth = NaN) {
     next(element, document.getElementsByClassName(c)[0])
-    // let sizes = getDimensions(element,c);
-    // if (isNaN(sizes.width)) next(element, c, i, 0);
-    // else next(element, c, i, sizes.width);
 }
 export function viewBefore(element = document, c = "", i=0, clientWidth = NaN) {
     let sizes = getDimensions(element,c);
@@ -28,14 +25,6 @@ export function viewBefore(element = document, c = "", i=0, clientWidth = NaN) {
     else before(element, c, i, sizes.width);
 }
 
-// function next(element = document, c = "", i=0, clientWidth = NaN) {
-//     let e = element.getElementsByClassName(c)[i];
-//     let posX = parseInt(e.style.left);
-//     let width = clientWidth == 0 ? parseInt(e.clientWidth) : clientWidth;
-//     if (posX <= (-e.scrollWidth + width)) e.style.left = `0px`;
-//     else e.style.left = `${(isNaN(posX)? 0 : posX) - width}px`;
-//     console.log(width, e.scrollWidth)
-// }
 function before(element = document, c = "", i=0, clientWidth = NaN) {
     if (posX == 0 || isNaN(posX)) e.style.left = `${(-e.scrollWidth + width)}px`;
     else e.style.left = `${(isNaN(posX)? 0 : posX) + width}px`;
@@ -53,21 +42,6 @@ function getDimensions(e = document.body, c = "") {
     let clientWidth = (values.articles * values.wArticle) + gap;
     return {"gap": gap, "width": clientWidth};
 }
-/*
-view next { 
-    Hara el control de las funciones, encapsulamiento. Solo se podra acceder a los demas metodos por medio de este
-}
-next { 
-    Contiene el codigo necesario para avanzar una imagen en el banner o una fila en los productos, 
-    este codigo debe tomar el cuenta medidas impares o con resto diferente a cero
-}
-getAttributes {
-    Este  codigo sera unico y no puede ser modificado, existe solo como complemento y varias funciones dependen de el.
-}
-
-
-
-*/
 
 function goNext() {
     getAttributes();
@@ -75,20 +49,39 @@ function goNext() {
 }
 
 function next(parent, object) {
-    // El tamaño del elemento que se movera no es divisible por si mismo, es decir. Dejar un resto diferente a cero.
-    // Si tiene resto CERO el elemento se movera siempre en saltos iguales
-    // Si el elemento resulta en resto diferente a cero, el elemento en su ultimo salto hara una multiplacacion por su 100% para obtener el movimiento faltante
-    let position, width,
-      childs = parent.childElementCount,
-    //   rest = object.offsetWidth / (width * childs);
-    rest = 0;
-    if (rest != 0) {
+    let wParent = parent.clientWidth;
+    let objectWidth = object.scrollWidth;
+    let childs = object.childElementCount;
+
+    let rest = ( object.firstChild.clientWidth * childs) % wParent;
+    console.log(rest, object.childElementCount, object.childNode);
+    let posX = object.offsetLeft;    
+
+
+    // if (rest != 0) {
+    //     let wArticle = object.firstChild.clientWidth;
+    //     let articles = Math.trunc(wParent / wArticle);
+    //     let gap = (wParent - (wArticle * articles) ) / articles;
         
-    }
-    else {
-        if ( (object.offsetLeft - object.offsetWidth) < (object.scrollWidth * -1)) 
-            object.style.left = `${object.offsetLeft - object.offsetWidth}px`;
-        // else ;
-        console.log( (object.offsetLeft - object.offsetWidth) < (object.scrollWidth * -1));
-    }
+    // }
+    // else {
+    //     if ( !((posX - wParent) < ( (objectWidth - wParent) * -1)) )
+    //         object.style.left = `${posX - wParent}px`;
+    // }   
+
 }
+
+
+// if ( (posX - width) + objectWidth >= wParent ) 
+// object.style.left = `${posX - width}px`;
+// else if ( 
+
+// posX - width == objectWidth
+
+// ) 
+
+// {
+// object.style.left = `${ posX - (objectWidth + (posX  - width)) - (gap / 2 )}px`; 
+// console.log( posX - (objectWidth + (posX  - width))  - (gap / 2) + objectWidth , wParent );
+// }
+// else object.style.left = '0px';
